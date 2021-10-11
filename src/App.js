412bbbdc5218dfda50d0f8514e8f67124409267c
VishnuPrasad115 <vishnuprasadkv55@gmail.com>
2021-10-11 11:06:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import Increment from "./components/increment";
 function App() {
+  const [hello, setHello] = useState("dark");
+  console.log("reinstantiated");
+  useEffect(() => {
+    console.log("rerendered");
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {hello} <Increment hello={hello} setHello={setHello} />
+    </>
   );
 }
 
---
+++ src/components/increment.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../actions';
+const Increment = ({setHello, hello})=>{
+    const counter = useSelector(state=> state.counter)
+    const isLogged = useSelector(state=> state.isLogged)
+    const dispatch = useDispatch();
+    console.log(hello)
+    return (
+      <div className="Hello">
+        <h1>Counter {counter}</h1>
+        <button onClick={()=> setHello('dark changed' + Math.random())} >change parent</button>
+        <button onClick={()=> dispatch(increment(5))} >+</button>
+        <button onClick={()=> dispatch(decrement())}>-</button>
+        {isLogged && <h3>Valuable information I shouldn't see</h3>}
+      </div>
+    );
+}
+
+export default Increment
